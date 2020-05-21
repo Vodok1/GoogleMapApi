ce77b806adb58d64b550c88ce55cf8f6fa8820e1
Jakub Bauza <bulzor95@gmail.com>
2020-05-21 15:19:54 +0200

    loadGeoJson use

--- maps.js
+++ maps.js
@@ -5,32 +5,21 @@ function initMap() {
     center: uluru,
     mapTypeId: "terrain",
   });
-
-  const xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      const response = JSON.parse(xhr.responseText);
-
-      for (let i = 0; i < response.features.length; i++) {
-        const coords = response.features[i].geometry.coordinates;
-        const latLng = new google.maps.LatLng(coords[1], coords[0]);
-        const magnitude = response.features[i].properties.mag;
-        const marker = new google.maps.Marker({
-          position: latLng,
-          map: map,
-          icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            fillColor: "red",
-            fillOpacity: 0.2,
-            scale: Math.pow(2, magnitude) / 2,
-            strokeColor: "white",
-            strokeWeight: 0.5,
-          },
-        });
-      }
-    }
-  };
-
-  xhr.open("GET", "./2.5_week.json", true);
-  xhr.send(null);
+  map.data.loadGeoJson("2.5_week.json");
+  map.data.setStyle(function (feature) {
+    var magnitude = feature.getProperty("mag");
+    return {
+      icon: getCircle(magnitude),
+    };
+  });
+  function getCircle(magnitude) {
+    return {
+      path: google.maps.SymbolPath.CIRCLE,
+      fillColor: "red",
+      fillOpacity: 0.2,
+      scale: Math.pow(2, magnitude) / 2,
+      strokeColor: "white",
+      strokeWeight: 0.5,
+    };
+  }
 }
